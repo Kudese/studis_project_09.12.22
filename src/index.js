797a4c15f91eb622efa8951546eb6dde50a6def7
Kudese <d.kuprichenkov@gmail.com>
2022-12-09 13:28:18 +0200

    ф

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import './js/movies-service'
+import './js/markup';
+import './js/movieModal';
+import { teamModal } from './js/teamModal.js';
+import { pagination } from './js/pagination.js';
+import './js/spinner';
+import { firebase } from './js/firebase.js';
+import './js/colorTheme';
+// import './js/addFilmLibrary';
---
+++ src/library.js
@@ -0,0 +1,5 @@
+import './js/teamModal.js';
+import './js/spinner';
+import './js/colorTheme';
+import './js/addFilmLibrary';
+import './js/movieModal';
